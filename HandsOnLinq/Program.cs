8baef98b1161a1862093922213eb293803656cf3
AcciGen <denrov13@gmail.com>
2024-02-21 09:13:03 +0500

    Task 1 complete

--- HandsOnLinq/Program.cs
+++ HandsOnLinq/Program.cs
@@ -4,7 +4,14 @@
 	{
 		private static void Main(string[] args)
 		{
-			
+			char c = 'a';
+			List<string> stringList = ["arcanera", "a", "armora", "arkane"];
+
+			var result = Tasks.Task1(c, stringList);
+			foreach(string res in result)
+			{
+				Console.WriteLine(res);
+			}
 		}
 	}
 }
